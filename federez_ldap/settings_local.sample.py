42d7d4de5351f46d76094fe54bc56895a5c4ffe4
Bertrand Bonnefoy-Claudet <bertrandbc@gmail.com>
2013-04-06 21:22:24 -0500

    Remove DATABASES from global settings

--- federez_ldap/settings_local.sample.py
+++ federez_ldap/settings_local.sample.py
@@ -3,6 +3,17 @@
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '',             # Or path to database file if using sqlite3.
+        'USER': '',             # Not used with sqlite3.
+        'PASSWORD': '',         # Not used with sqlite3.
+        'HOST': '',             # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',             # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '&amp;%_)uh4i3u%7fmcm98$tu+r03619eb!qqrwzhk92%eyl0@tdt5'
 
